6eafa0dc05421a03d8ce9c87bd872ff1e37efc25
Ibad Mughal <ibadamir3@gmail.com>
2023-03-09 15:41:13 +0500

    First Ever WordCloud using python

--- file_read.py
+++ file_read.py
@@ -1,16 +1,15 @@
 # import csv
-# from collections import Counter
-# from wordcloud import WordCloud
+from collections import Counter
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
-
-# # Job 1: Read keyword column and assign data to a data-structure(list)
-keywords_list = []
+# Job 1: Read keyword column and assign data to a data-structure(list)
+keywordsList = []
 
 # Method 1
 file = open('dataset-medical.csv', "r", encoding="utf-8")
 for line in file:
-    keywords_list.append(line.rstrip().split(",")[1])
+    keywordsList.append(line.rstrip().split(",")[1])
     
 # Method 2    
 # with open('dataset-medical.csv', 'r', newline='', encoding='utf-8') as csvfile:
@@ -21,9 +20,11 @@ for line in file:
 
 # Job 2: Separate words using ';' separator
 keywords = []
-for words in keywords_list:
-    keywords.extend(words.split(';'))
+for words in keywordsList:
+    element = [word.upper() for word in words.split(";")]
+    keywords.extend(element)
 
+print(keywords)
 # Job 3: Count Frequency of each word (occurrence of word in entire dataset)
 word_freq = {}
 for word in keywords:
@@ -37,11 +38,11 @@ for word in keywords:
 
 
 # # Job 4: Create wordcloud from word frequency dictionary using WordCloud library
-# wordcloud = WordCloud(width=800, height=800, background_color='white').generate_from_frequencies(word_freq)
+wordcloud = WordCloud(width=800, height=800, background_color='white').generate_from_frequencies(Counter(keywords))
 
 # Job 5: Display wordcloud using matplotlib
 plt.figure(figsize=(8, 8), facecolor=None)
-plt.imshow(word_freq)
+plt.imshow(wordcloud)
 plt.axis('off')
 plt.tight_layout(pad=0)
 plt.show()
